3e5a670c0839bd4875c1d2a216fd53029c7d6efb
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-30 15:03:46 +0200

    feat: increase slug by 1 if included

--- createSlug.js
+++ createSlug.js
@@ -1,10 +1,19 @@
-const posts = require("./postsDb.json");
-
 const createSlug = (title, posts) => {
   if (typeof title !== "string") {
     throw new Error("title must be a string");
   }
-  return title.toLowerCase().replaceAll(" ", "-");
+
+  const slugs = posts.map((post) => post.slug);
+  const baseSlug = title.toLowerCase().replaceAll(" ", "-");
+  let slug = baseSlug;
+
+  let count = 1;
+  while (slugs.includes(slug)) {
+    slug = `${baseSlug}-${count}`;
+    count++;
+  }
+
+  return slug;
 };
 
 module.exports = {
--- tests/createSlug.test.js
+++ tests/createSlug.test.js
@@ -1,16 +1,21 @@
 const { expect, it } = require("@jest/globals");
 const { createSlug } = require("../createSlug");
+const posts = require("../postsDb.json");
 
 it("should return a string", () => {
   expect(() => {
-    createSlug(1);
+    createSlug(1, posts);
   }).toThrow;
 });
 
 it("should return a lowercase string", () => {
-  expect(createSlug("CIAO")).toBe("ciao");
+  expect(createSlug("CIAO", posts)).toBe("ciao");
 });
 
 it("should return a string with a dash instead of a space", () => {
-  expect(createSlug("Ciao ciAo")).toBe("ciao-ciao");
+  expect(createSlug("Ciao ciAo", posts)).toBe("ciao-ciao");
+});
+
+it("should increase slug by one if slug already exists", () => {
+  expect(createSlug("ciambellone", posts)).toBe("ciambellone-1");
 });
